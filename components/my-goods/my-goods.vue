f1ab45ab4cd9a175099140f9b73d02fbf7147007
Echo021 <chenxiaomin301@163.com>
2023-06-03 17:07:34 +0800

    goodslist complete

---
+++ components/my-goods/my-goods.vue
@@ -0,0 +1,64 @@
+<template>
+		<view class="goods_item">
+			<view class="left_goods_image">
+				<image :src="good.goods_small_logo"></image>
+			</view>
+			<view class="right_goods_info">
+				<view class="good_desc">{{good.goods_name}}</view>
+				<view class="good_price">￥{{good.goods_price | tofixed}}</view>
+			</view>
+		</view>
+</template>
+
+<script>
+	export default {
+		name:"my-goods",
+		props:{
+			good:{
+				type: Object,
+				default: {}
+			}
+		},
+		data() {
+			return {
+				
+			};
+		},
+		filters:{
+			tofixed(num){
+				return Number(num).toFixed(2)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+$color: #ff6d00;
+	.goods_item{
+		display: flex;
+		margin-bottom: 5px;
+		
+		.left_goods_image{
+			margin-right: 5px;
+			
+			image{
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+		
+		.right_goods_info{
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			
+			.good_desc{
+				font-size: 13px;
+			}
+			.good_price{
+				color: $color
+			}
+		}
+}
+</style>
--- package/good_list/good_list.vue
+++ package/good_list/good_list.vue
@@ -1,6 +1,8 @@
 <template>
-	<view>
-		goods_list
+	<view class="goods_list">
+		<view v-for="(good,index) in goodsList" :key="good.cat_id" @click="gotoDetail(good.cat_id)">
+			<my-goods :good="good"></my-goods>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +10,54 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj:{
+					query: '',
+					cid: '',
+					pagenum: 1,
+					pageSize: 10
+				},
+				total: 0,
+				goodsList:[],
+				loading: false
 			};
+		},
+		onLoad(option){
+			this.queryObj.query=option.query || '';
+			this.queryObj.cid=option.cid || '';
+			this.getGoodsListData()
+		},
+		methods:{
+			async getGoodsListData(){
+				this.loading=true
+				const {data:res} = await uni.$http.get('/api/public/v1/goods/search',this.queryObj);
+				this.loading = false
+				if(res.meta.status!=200) return uni.$showMessage()
+				this.goodsList=[...res.message.goods,...this.goodsList];
+				this.total=res.message.total
+			},
+			gotoDetail(id){
+				uni.navigateTo({
+					url: `/package/goods_detail/goods_detail?goods_id=${id}`
+				})
+			}
+		},
+		onReachBottom() {
+			if(this.queryObj.pagenum*this.queryObj.pageSize>=this.total) return uni.$$showMessage('数据加载完毕！')
+			if(this.loading) return;
+			this.queryObj.pagenum++
+			this.getGoodsListData()
+		},
+		onPullDownRefresh(){
+			this.queryObj.pagenum = 1;
+			this.goodsList=[];
+			this.total=0;	
+			this.getGoodsListData()
 		}
 	}
 </script>
 
 <style lang="scss">
-
+.goods_list{
+	margin: 10px 5px;
+}
 </style>
